af7daf16d13bdc565f43623fbff714c1f8a09fb1
Przemo_Poz <przemyslaw.tusz@gmail.com>
2018-10-07 10:22:37 +0200

    takie tam1

--- zjazd_1/zad14.py
+++ zjazd_1/zad14.py
@@ -5,7 +5,7 @@ liczba = 0
 print(f"""Wpisz kolejną liczbę do obliczenia statystyk - zakończ wpisując x""")
 while True:
     liczba = input(f"""Podaj {i}-tą liczbę: """)
-    if liczba == "x":
+    if liczba == "x":# or type(liczba)!=float or type(liczba)!=int:
         liczba = 0
         break
     else:
@@ -24,6 +24,6 @@ while True:
         suma = suma + liczba
         i=i+1
 
-print(f"""Średnia dla {i-1} liczb to {round(suma/(i),2)}
+print(f"""Średnia dla {i-1} liczb to {round(suma/(i-1),2)}
 Największa liczba była w pisania przez Ciebie w {imini} iteracji, gdy podałeś {mini}
 Największa liczba była w pisania przez Ciebie w {imaks} iteracji, gdy podałeś {maks}""")
