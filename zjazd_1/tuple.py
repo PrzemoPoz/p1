0d8502c3c01396d8e3b93bbce8265df021d6c6ca
Przemo_Poz <przemyslaw.tusz@gmail.com>
2018-10-07 12:46:47 +0200

    takie tam1

---
+++ zjazd_1/tuple.py
@@ -0,0 +1,12 @@
+#10 liczb całk
+#drugi, przedostatni, od 3 do 7 (włącznie), co 3., c 2. licząc od końca
+
+
+cionk=(1,2,3,4,5,6,7,8,9,10)
+
+print(cionk[1])
+print(cionk[-2])
+print(cionk[2:8])
+print(cionk[::3])
+print(cionk[::-2])
+print("długość to",len(cionk))
