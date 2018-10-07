44e723b486f4bbf01fdc6eefee01bbc1eed6095f
Przemo_Poz <przemyslaw.tusz@gmail.com>
2018-10-07 13:00:13 +0200

    takie tam1

---
+++ zjazd_1/listy.py
@@ -0,0 +1,14 @@
+cionk = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(cionk[1])
+print(cionk[-2])
+print(cionk[2:8])
+print(cionk[::3])
+print(cionk[::-2])
+print("długość to", len(cionk))
+
+#dodawanie do listy
+
+cionk.append(11)
+print(cionk)
+print(sum(cionk))
--- zjazd_1/tuple.py
+++ zjazd_1/tuple.py
@@ -1,12 +1,16 @@
-#10 liczb całk
-#drugi, przedostatni, od 3 do 7 (włącznie), co 3., c 2. licząc od końca
-
-
-cionk=(1,2,3,4,5,6,7,8,9,10)
+# 10 liczb całk
+# drugi, przedostatni, od 3 do 7 (włącznie), co 3., c 2. licząc od końca
+cionk = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 print(cionk[1])
 print(cionk[-2])
 print(cionk[2:8])
 print(cionk[::3])
 print(cionk[::-2])
-print("długość to",len(cionk))
+print("długość to", len(cionk))
+
+#dodawanie do tupli
+
+cionk1=(cionk,12)
+print(cionk1)
+
