230aaf41a7ad8d065ca11410fb989c04723afe45
Przemo_Poz <przemyslaw.tusz@gmail.com>
2018-10-20 09:19:16 +0200

    takie tam1

--- zjazd_1/kolekcje/zad7.py
+++ zjazd_1/kolekcje/zad7.py
@@ -1,5 +1,5 @@
 slowo=input("Podaj słowo, w kórym zliczę samogłoski: ")
 
-
+slowo=slowo.lower()
 licz=slowo.count('a')+slowo.count('e')+slowo.count('i')+slowo.count('o')+slowo.count('u')+slowo.count('y')
 print(f"""W słowie {slowo} znajduje się {licz} samogłosek""")
--- zjazd_1/kolekcje/zad9.py
+++ zjazd_1/kolekcje/zad9.py
@@ -1,8 +1,9 @@
-slownik={'ziemniaki':1.5,'jablka':2.5,'xx':1}
+slownik={'ziemniaki':1.5,'jabłka':2.5,'xx':1}
 
 co=input("wpisz co kupujesz: ")
 ile=float(input("wpisz ile kilosów: "))
 siano=round(float(slownik[co])*ile,2)
+print(slownik)
 print(f"""Cena za {co} to {slownik[co]} za kg, zatem należność wynosi {siano} PLN""")
 
 
